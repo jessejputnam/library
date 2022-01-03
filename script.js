782eb396a56cde1b5d9292b43f0a7668fa69d03a
Jesse Putnam <jessejputnam@gmail.com>
2022-01-02 20:29:21 -0500

    style the library

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 "use strict";
 
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = () => {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${read}`;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${
+      read === true ? "finished reading" : "not read yet"
+    }`;
   };
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+function addBooktoLibrary(book) {
+  return myLibrary.push(book);
+}
 
-console.log(theHobbit.read);
+const theHobbit = new Book("The Hobbit", "Tolkien", 295, true);
+const paleFire = new Book("Pale Fire", "Nabokov", 300, true);
+const darknessAtNoon = new Book("Darkness at Noon", "Koestler", 170, false);
+const fledgling = new Book("Fledgling", "Butler", 230, true);
+
 console.log(theHobbit.info());
+console.log(fledgling.info());
+
+addBooktoLibrary(theHobbit);
+addBooktoLibrary(paleFire);
+addBooktoLibrary(darknessAtNoon);
+addBooktoLibrary(fledgling);
+
+let sortedLibrary = myLibrary.sort((a, b) => (a.author > b.author ? 1 : -1));
+
+console.table(sortedLibrary);
