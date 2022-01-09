cf79a09bb03d2bb0d3b449d71f52c63ab5eb83dc
Jesse Putnam <jessejputnam@gmail.com>
2022-01-09 18:58:48 -0500

    Add button and modal visual functions

--- script.js
+++ script.js
@@ -1,5 +1,84 @@
 "use strict";
+/*
+ 
+Table of Contents:
 
+#DOM Variables
+#Add Book Modal
+#Book Card
+#Add Book to Library
+#Original Book Code
+
+*/
+
+////////////////////////////////////////////////
+
+/* ########## #DOM Variables ############ */
+// Banner
+const btnAddBook = document.querySelector(".banner__add");
+// Book Shelf
+const shelf = document.querySelectorAll(".shelf");
+const shelfActive = document.querySelector(".shelf__used");
+// Add Book Modal
+const modalAddBook = document.querySelector(".modal__add-book");
+const modalClose = document.querySelector(".modal__close");
+const btnAddToLibrary = document.querySelector(".modal__add--btn");
+// Book Card
+const bookCard = document.querySelector(".book__card");
+const bookCardClose = document.querySelector(".card__close");
+
+/* ########## #Add Book Modal ############ */
+//Open Modal
+const openAddBookModal = function () {
+  modalAddBook.classList.remove("hidden");
+};
+btnAddBook.addEventListener("click", openAddBookModal);
+
+//Close Modal
+modalClose.addEventListener("click", () => {
+  modalAddBook.classList.add("hidden");
+});
+
+/* ########## #Book Card ############ */
+//Open Card
+// const openBookCard = function () {
+//   bookCard.classList.remove("hidden");
+// };
+
+// bookSelect.forEach((book) => {
+//   book.addEventListener("click", openBookCard);
+// });
+
+//Close Card
+bookCardClose.addEventListener("click", () => {
+  bookCard.classList.add("hidden");
+});
+
+/* ########## #Add Book to Library ############ */
+btnAddToLibrary.addEventListener("click", () => {
+  //create library book visual
+  const spine = document.createElement("div");
+  const spineTitle = document.createElement("p");
+  spineTitle.textContent = document.getElementById("btitle").value;
+  spine.classList.add("book");
+  spineTitle.classList.add("book__title");
+  spine.appendChild(spineTitle);
+  shelfActive.appendChild(spine);
+
+  //close window
+  modalAddBook.classList.add("hidden");
+
+  //open book card on created book
+  const openBookCard = function () {
+    bookCard.classList.remove("hidden");
+  };
+
+  document.querySelectorAll(".book").forEach((book) => {
+    book.addEventListener("click", openBookCard);
+  });
+});
+
+/* ########## #Original Book Code ############ */
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -24,7 +103,6 @@ const darknessAtNoon = new Book("Darkness at Noon", "Koestler", 170, false);
 const fledgling = new Book("Fledgling", "Butler", 230, true);
 
 console.log(theHobbit.info());
-console.log(fledgling.info());
 
 addBooktoLibrary(theHobbit);
 addBooktoLibrary(paleFire);
